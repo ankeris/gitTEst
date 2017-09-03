d9bd700c168122bdd58a60211f95910dad854db5
JuozasR <31132643+ankeris@users.noreply.github.com>
2017-09-03 17:27:36 +0200

    Update sketch.js

--- lightOn/sketch.js
+++ lightOn/sketch.js
@@ -3,11 +3,11 @@ function setup() {
 }
 
 var lightOn = true;
-function draw() {
-  if (lightOn) {
-    fill(255,255,0); background(255);
-  } else {
-    fill(0); background(123);
+ function draw() {
+   if (lightOn) {
+     fill(255,255,0); background(255);
+   } else {
+     fill(0); background(123);
   }
 
   rect(260,160,80,80);
