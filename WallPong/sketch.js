f6e9045b8cc73730ac19a1cf0580616ce0d7812b
JuozasR <31132643+ankeris@users.noreply.github.com>
2017-09-30 22:18:08 +0200

    fix

--- WallPong/sketch.js
+++ WallPong/sketch.js
@@ -1,36 +1,51 @@
 var points = 0;
+var myCanvas;
+var balls = [];
+var playRect;
+var startGame = "Press here to spawn a ball";
+
 function pointsCounter(){
+	textAlign(LEFT);
 	text(points, 10, 30);
+	textSize(12);
+	textAlign(CENTER);
+	text(startGame, width/2, height/2);
 	fill(255);
 }
 
-var playBall = new Ball(55, 155);
-var playRect = new Rectangle(160, 250, 50, 10, 2, 2);
-
 function setup(){
-	createCanvas(260,260);
+	myCanvas = createCanvas(260,260);
+	playRect = new Rectangle(160, 250, 50, 10, 2, 2);
 }
 
+// Ball appears;
+function mousePressed() {
+	if (balls.length < 3){
+		balls.push(new Ball(mouseX, mouseY));
+	}
+	startGame = "";
+}
+
+
 function draw(){
-	
 	background(100);
 	fill(255);
 	textSize(16);
-	text("controll with right/left arrow", 10, 50);
+	textAlign(LEFT);
+	text("control with right/left arrow", 10, 50);
 	fill(22,180,200);
 	textSize(22);
 	pointsCounter();
-//----- ball properties
-	playBall.display();
-	playBall.move();
-	playBall.ballWallHit();
-	playBall.bounceOfRectangle();
 
-//----- rectangle properties
 	playRect.displayRect();
-	playRect.keyPressed();
+	playRect.keyIsDown();
 	playRect.fixed();
-	
+	for (var i = 0; i < balls.length; i++){
+		balls[i].display();
+		balls[i].move();
+		balls[i].ballWallHit();
+		balls[i].bounceOfRectangle();
+	}
 }
 
 function Rectangle(xas, yas, w, h, xspeed, yspeed){
@@ -48,16 +63,16 @@ function Rectangle(xas, yas, w, h, xspeed, yspeed){
 	}
 
 	// movement by arrows
-	this.keyPressed = function(){
-		if (keyCode == RIGHT_ARROW) {
+	this.keyIsDown = function(){
+		if (keyIsDown(RIGHT_ARROW)) {
 		this.x = this.x + this.xspeed;
 		} 
-		else if (keyCode == LEFT_ARROW) {
+		else if (keyIsDown(LEFT_ARROW)) {
 		this.x = this.x - this.xspeed;
 		}
 	}
 
-	// if ball hits the wall it stops
+	// if rectangle hits the wall it stops
 	this.fixed = function(){
 		this.x = constrain(this.x, 25, width-25);
 	}
@@ -74,11 +89,11 @@ function Ball(x, y){
         fill(255);
         ellipse(this.x, this.y, this.r, this.r);
     }
-
     // speed of the ball
     this.move = function() {
     	this.x = this.x + this.xspeed;
     	this.y = this.y + this.yspeed;
+    	console.log(this.y);
     }
     // if it hits the wall it goes other direction
     this.ballWallHit = function(){
@@ -92,16 +107,20 @@ function Ball(x, y){
 
     // when Ball hits the rectangle it bounces back and gets the point
     this.bounceOfRectangle = function(){
-    	if (this.y > 240 && this.x < playRect.x + playRect.w/2 && this.x > playRect.x - playRect.w/2){
+    	if ((this.y == 240 ||this.y == 241 ||this.y == 242) && this.x < playRect.x + playRect.w/2 && this.x > playRect.x - playRect.w/2){
     		this.yspeed = this.yspeed * -1;
+
     		//adds +1 point on hit
     		points = points + 1;
     	}
 
-    	if (this.y > 280) {
-    		this.yspeed = 0;
-    		points = points + "  Good luck next time!";
+    	if (this.y > 290) {
+    		balls.splice(0,balls.length);
+    		points = 0;
     		fill(255,0,0);
     	}
     }
 }
+
+
+    	
